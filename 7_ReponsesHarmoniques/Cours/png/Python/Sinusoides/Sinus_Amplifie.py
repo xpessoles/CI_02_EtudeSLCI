5b42e53d95daec89d87132c67648aeec64ac4b2c
xpessoles <xpessoles.ptsi@free.fr>
2014-01-02 21:12:39 +0100

    Ajout des courbes

--- 7_ReponsesHarmoniques/Cours/png/Python/Sinusoides/Sinus_Amplifie.py
+++ 7_ReponsesHarmoniques/Cours/png/Python/Sinusoides/Sinus_Amplifie.py
@@ -4,18 +4,18 @@ from math import sqrt
 
 
 
-x=np.linspace(1,20,500)
+x=np.linspace(0,20,500)
 p1=plt.plot(x,(np.sin(x)),
-            label="s(t)",linewidth=3)
+            label="$\sin(t)$",linewidth=3)
 p2=plt.plot(x,(0.75*np.sin(x)),
-            label="s(t)",linewidth=3)
+            label="$0,75\cdot\sin(t)$",linewidth=3)
 
 plt.grid(True, which="both", linestyle="dotted")
 #plt.title("Fonctions trigonometriques")  
 #plt.legend([p1, p2], ["Sinus", "Cosinus"])
 plt.legend(loc='lower right', fancybox=True, shadow=True, prop=dict(size=10))
 plt.xlabel("Temps en $s$")
-plt.ylabel("Position $y(t)$ en $m$")
+plt.ylabel("$f(t)$")
 plt.axis([0,20,-1.5,1.5])
 plt.show()
 
