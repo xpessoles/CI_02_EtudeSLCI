b262dad72b0a0e81df9c9cb947043ffcc0f3d37d
Xavier Pessoles <xpessoles.ptsi@free.fr>
2014-03-29 06:49:42 +0100

    Exo diagramme de Bode

--- 7_ReponsesHarmoniques/Applications_02/png/Python/Exemple1 - Copie.py
+++ 7_ReponsesHarmoniques/Applications_02/png/Python/Exemple1 - Copie.py
@@ -2,37 +2,32 @@ import matplotlib.pyplot as plt
 import numpy as np
 from math import sqrt
 from math import pi
+from math import log10
 
-#EXERCICE RADAR
+xf=100
+omega=np.linspace(0.01,xf,10000)
 
-xi=0.01
-xf=1000
-ai = -60
-af = 10
-phii = -2*pi
-phif=pi
-omega=np.linspace(xi,xf,1000)
-
+K,E0,tau,om0 = 20,1,1,0.01
 
 
 plt.subplot(2,1,1)
-
+plt.plot([0.01,100],[20*log10(10),20*log10(10)],
+         label="$Adb(\\omega)$",linewidth=3)
 plt.ylabel("Gain $Gdb$")
 plt.xlabel("Pulsation $\\omega$")
 plt.grid(True, which="both", linestyle="dotted")
 plt.legend(loc='lower right', fancybox=True, shadow=True, prop=dict(size=10))
 plt.semilogx()
-plt.axis([xi,xf,ai,af])
+
 plt.subplot(2,1,2)
-#plt.plot(omega,
-#         -np.arctan(tau*omega),
-#         label="$\\varphi(\\omega)$",linewidth=3)
+plt.plot(omega,
+         -np.arctan(tau*omega),
+         label="$\\varphi(\\omega)$",linewidth=3)
 plt.ylabel("Phase $rad$")
 plt.grid(True, which="both", linestyle="dotted")
 plt.legend(loc='lower right', fancybox=True, shadow=True, prop=dict(size=10))
 plt.semilogx()
-plt.axis([xi,xf,phii,phif])
-
+#plt.axis([0.01,xf,0,1.6])
 #plt.title("Fonctions trigonometriques")  
 #plt.legend([p1, p2], ["Sinus", "Cosinus"])
 #plt2.legend(loc='upper right', fancybox=True, shadow=True, prop=dict(size=10))
